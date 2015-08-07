6f0ef353c3bcac3c5825ab889b2713817d41c96f
minodisk <daisuke.mino@gmail.com>
2015-08-07 13:25:36 +0900

    Babelize

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,26 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var jade = require('gulp-jade');
-var webpack = require('webpack');
-ref = require('path'), join = ref.join, dirname = ref.dirname, basename = ref.basename, extname = ref.extname, relative = ref.relative, resolve = ref.resolve;
-ref1 = require('fs'), readFileSync = ref1.readFileSync, writeFileSync = ref1.writeFileSync, mkdir = ref1.mkdir;
-var packager = require('electron-packager');
-var GitHub = require('github');
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var Q = require('q');
-var argv = require('yargs').argv;
-var semver = require('semver');
-var livereload = require('gulp-livereload');
+import gulp from 'gulp'
+import gutil from 'gulp-util'
+import plumber from 'gulp-plumber'
+import jade from 'gulp-jade'
+import webpack from 'webpack'
+import {join, dirname, basename, extname, relative, resolve} from 'path'
+import {readFileSync, writeFileSync, mkdir} from 'fs'
+import packager from 'electron-packager'
+import GitHub from 'github'
+import {spawn} from 'child_process'
+import {exec} from 'child_process'
+import Q from 'q'
+import argv from 'yargs'
+import semver from 'semver'
+import livereload from 'gulp-livereload'
 
-var pkg = JSON.parse(readFileSync('package.json'));
-var url = pkg.repository.url;
-var owner = basename(dirname(url));
-var repo = basename(pkg.repository.url, extname(url));
-var electron = null;
-var isWatch = false;
+let pkg = JSON.parse(readFileSync('package.json'));
+let url = pkg.repository.url;
+let owner = basename(dirname(url));
+let repo = basename(pkg.repository.url, extname(url));
+let isWatch = false;
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   livereload.listen({
     basePath: 'dist',
     start: true
@@ -30,40 +29,39 @@ gulp.task('default', function() {
   return gulp.start('debug');
 });
 
-gulp.task('debug', ['build'], function() {
+gulp.task('debug', ['build'], () => {
   return gulp.start('electron');
 });
 
-gulp.task('electron', function() {
-  electron = spawn('./node_modules/electron-prebuilt/cli.js', ['dist']);
-  electron.stdout.on('data', function(data) {
-    return console.log(data.toString('utf-8'));
-  });
-  electron.stderr.on('data', function(data) {
-    return console.log(data.toString('utf-8'));
-  });
-  return electron.on('close', function(code) {
-    return console.log('Electron done');
+gulp.task('electron', (cb) => {
+  exec('./node_modules/electron-prebuilt/cli.js dist', (err, stdout, stderr) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    if (stdout) console.log(stdout);
+    if (stderr) console.error(stderr);
+    cb();
   });
 });
 
-gulp.task('package', ['build'], function (cb) {
+gulp.task('package', ['build'], (cb) => {
   icon()
   .then(pack)
-  .then(function (dirs) {
+  .then((dirs) => {
     cb(null, dirs);
   })
-  .fail(function (err) {
-    console.error(err);
+  .fail((err) => {
+    cb(err)
   });
 });
 
 function icon() {
   return Q
   .when('')
-  .then(function () {
-    var d = Q.defer();
-    exec('iconutil -c icns Markn.iconset', {cwd: 'assets'}, function (err, stdout, stderr) {
+  .then(() => {
+    let d = Q.defer();
+    exec('iconutil -c icns Markn.iconset', {cwd: 'assets'}, (err, stdout, stderr) => {
       if (err) {
         d.reject(err);
         return;
@@ -74,9 +72,22 @@ function icon() {
     });
     return d.promise;
   })
-  .then(function () {
-    var d = Q.defer();
-    exec('convert Markn.iconset/icon_16x16.png Markn.iconset/icon_32x32.png Markn.iconset/icon_128x128.png Markn.iconset/icon_256x256.png Markn.iconset/icon_512x512.png Markn.ico', {cwd: 'assets'}, function (err, stdout, stderr) {
+  .then(() => {
+    let d = Q.defer();
+    exec('iconutil -c icns Markn.iconset', {cwd: 'assets'}, (err, stdout, stderr) => {
+      if (err) {
+        d.reject(err);
+        return;
+      }
+      if (stdout) console.log(stdout);
+      if (stderr) console.error(stderr);
+      d.resolve();
+    });
+    return d.promise;
+  })
+  .then(() => {
+    let d = Q.defer();
+    exec('convert Markn.iconset/icon_16x16.png Markn.iconset/icon_32x32.png Markn.iconset/icon_128x128.png Markn.iconset/icon_256x256.png Markn.iconset/icon_512x512.png Markn.ico', {cwd: 'assets'}, (err, stdout, stderr) => {
       if (err) {
         d.reject(err);
         return;
@@ -90,7 +101,7 @@ function icon() {
 }
 
 function pack() {
-  var d;
+  let d;
   d = Q.defer();
   packager({
     dir: 'dist',
@@ -101,7 +112,7 @@ function pack() {
     version: '0.30.2',
     icon: 'assets/Markn',
     overwrite: true
-  }, function(err, dirs) {
+  }, (err, dirs) => {
     if (err != null) {
       d.reject(err);
       return;
@@ -114,7 +125,7 @@ function pack() {
 
 gulp.task('build', ['copy', 'jade', 'webpack']);
 
-gulp.task('copy', function() {
+gulp.task('copy', () => {
   return gulp.src([
     'package.json',
     'README.md',
@@ -127,15 +138,15 @@ gulp.task('copy', function() {
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('jade', function() {
-  gulp
+gulp.task('jade', () => {
+  return gulp
   .src('src/renderer/index.jade')
   .pipe(jade())
   .pipe(gulp.dest('./dist'))
   .pipe(livereload());
 });
 
-gulp.task('webpack', function(cb) {
+gulp.task('webpack', (cb) => {
   webpack({
     watch: isWatch,
     entry: {
@@ -179,17 +190,17 @@ gulp.task('webpack', function(cb) {
       extensions: ['', '.js', '.coffee']
     },
     externals: [
-      (function() {
-        var IGNORE;
+      (() => {
+        let IGNORE;
         IGNORE = [
           // Node
           'fs',
           'path',
           // Electron
           'crash-reporter', 'app', 'menu', 'menu-item', 'browser-window', 'dialog', 'shell', 'ipc', 'chokidar'];
-        return function(context, request, callback) {
+        return (context, request, callback) => {
           if (IGNORE.indexOf(request) >= 0) {
-            return callback(null, "require('" + request + "')");
+            return callback(null, `require('${request}')`);
           }
           return callback();
         };
@@ -207,7 +218,7 @@ gulp.task('webpack', function(cb) {
       __dirname: false,
       setImmediate: false
     }
-  }, function(err, stats) {
+  }, (err, stats) => {
     if (err != null) {
       throw new gutil.PluginError('webpack', err);
     }
@@ -222,9 +233,9 @@ gulp.task('webpack', function(cb) {
   });
 });
 
-gulp.task('release', ['build'], function() {
-  var github = new GitHub({
-    version: "3.0.0",
+gulp.task('release', ['build'], () => {
+  let github = new GitHub({
+    version: '3.0.0',
     debug: true
   });
   github.authenticate({
@@ -233,14 +244,14 @@ gulp.task('release', ['build'], function() {
   });
 
   Q.when('')
-  .then(function() {
-    var d;
+  .then(() => {
+    let d;
     d = Q.defer();
     mkdir('dist', d.resolve);
     return d.promise;
   })
-  .then(function() {
-    var json, release, releases, version;
+  .then(() => {
+    let json, release, releases, version;
     releases = ['major', 'minor', 'patch'];
     release = argv.r;
     if (releases.indexOf(release) < 0) {
@@ -252,84 +263,84 @@ gulp.task('release', ['build'], function() {
     [
       'package.json',
       'dist/package.json'
-    ].forEach(function(p) {
+    ].forEach((p) => {
       return writeFileSync(p, json);
     });
   })
-  .then(function() {
+  .then(() => {
     return console.log('bump package.json');
   })
-  .then(function() {
-    var d, git;
+  .then(() => {
+    let d, git;
     d = Q.defer();
-    console.log("git commit package.json");
-    git = spawn('git', ['commit', '-m', "Bump version to v" + pkg.version, 'package.json']);
-    git.stdout.on('data', function(data) {
+    console.log('git commit package.json');
+    git = spawn('git', ['commit', '-m', `Bump version to v${pkg.version}`, 'package.json']);
+    git.stdout.on('data', (data) => {
       return console.log(data.toString('utf8'));
     });
-    git.stderr.on('data', function(data) {
+    git.stderr.on('data', (data) => {
       return console.log(data.toString('utf8'));
     });
-    git.on('close', function(code) {
+    git.on('close', (code) => {
       return d.resolve();
     });
     return d.promise;
   })
-  .then(function() {
-    var d, git;
+  .then(() => {
+    let d, git;
     d = Q.defer();
-    console.log("git push");
+    console.log('git push');
     git = spawn('git', ['push']);
-    git.stdout.on('data', function(data) {
+    git.stdout.on('data', (data) => {
       return console.log(data.toString('utf8'));
     });
-    git.stderr.on('data', function(data) {
+    git.stderr.on('data', (data) => {
       return console.log(data.toString('utf8'));
     });
-    git.on('close', function(code) {
+    git.on('close', (code) => {
       return d.resolve();
     });
     return d.promise;
   })
   .then(icon)
   .then(pack)
-  .then(function(dirs) {
-    dirs = dirs.map(function(dir) {
-      conosle.log(dir)
-      var name = basename(dir);
-      var zip = name + ".zip";
+  .then((dirs) => {
+    dirs = dirs.map((dir) => {
+      console.log(dir)
+      let name = basename(dir);
+      let zip = name + '.zip';
       return {
         dir: dir,
         name: name,
         zip: zip
       };
     });
-    return Q.all(dirs.map(function(arg) {
-      var d, name, zip;
+    return Q.all(dirs.map((arg) => {
+      let d, name, zip;
       name = arg.name, zip = arg.zip;
       d = Q.defer();
-      console.log("zip " + name + " to " + zip);
+      console.log('zip ' + name + ' to ' + zip);
       zip = spawn('zip', [zip, '-r', name], {cwd: 'tmp'});
-      zip.stdout.on('data', function(data) {
+      zip.stdout.on('data', (data) => {
         return console.log(data.toString('utf8'));
       });
-      zip.stderr.on('data', function(data) {
+      zip.stderr.on('data', (data) => {
         return console.log(data.toString('utf8'));
       });
-      zip.on('close', function(code) {
+      zip.on('close', (code) => {
         return d.resolve();
       });
       return d.promise;
     }))
-    .then(function() {
-      var d;
+    .then(() => {
+      let d;
       d = Q.defer();
       console.log('create new release');
       github.releases.createRelease({
         owner: owner,
         repo: repo,
-        tag_name: "v" + pkg.version
-      }, function(err, res) {
+        tag_name: 'v' + pkg.version
+      }, (err, res) => {
         if (err != null) {
           d.reject(err);
         }
@@ -338,10 +349,10 @@ gulp.task('release', ['build'], function() {
       });
       return d.promise;
     })
-    .then(function(id) {
-      console.log("complete to create new release: " + id);
-      return Q.all(dirs.map(function(arg) {
-        var d, zip;
+    .then((id) => {
+      console.log('complete to create new release: ' + id);
+      return Q.all(dirs.map((arg) => {
+        let d, zip;
         zip = arg.zip;
         d = Q.defer();
         github.releases.uploadAsset({
@@ -350,7 +361,7 @@ gulp.task('release', ['build'], function() {
           id: id,
           name: zip,
           filePath: 'tmp' + zip
-        }, function(err, res) {
+        }, (err, res) => {
           if (err != null) {
             d.reject(err);
           }
@@ -359,9 +370,15 @@ gulp.task('release', ['build'], function() {
         });
         return d.promise;
       }))
-      .then(function() {
+      .then(() => {
         return console.log('complete to upload');
       });
     });
+  })
+  .then(() => {
+    console.log('done');
+  })
+  .fail((err) => {
+    console.error(err);
   });
 });
